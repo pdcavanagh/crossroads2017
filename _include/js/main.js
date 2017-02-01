17b205977f5f76145d9c9d33697de45013eb6057
Patrick Cavanagh <pdcavanagh@yahoo.com>
2017-01-31 23:56:43 -0500

    clean version for review

--- _include/js/main.js
+++ _include/js/main.js
@@ -46,59 +46,59 @@ BRUSHED.listenerMenu = function(){
    Slider Options
 ================================================== */
 
-BRUSHED.slider = function(){
-	$.supersized({
-		// Functionality
-		slideshow               :   1,			// Slideshow on/off
-		autoplay				:	1,			// Slideshow starts playing automatically
-		start_slide             :   1,			// Start slide (0 is random)
-		stop_loop				:	0,			// Pauses slideshow on last slide
-		random					: 	0,			// Randomize slide order (Ignores start slide)
-		slide_interval          :   12000,		// Length between transitions
-		transition              :   1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
-		transition_speed		:	300,		// Speed of transition
-		new_window				:	1,			// Image links open in new window/tab
-		pause_hover             :   0,			// Pause slideshow on hover
-		keyboard_nav            :   1,			// Keyboard navigation on/off
-		performance				:	1,			// 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
-		image_protect			:	1,			// Disables image dragging and right click with Javascript
+// BRUSHED.slider = function(){
+// 	$.supersized({
+// 		// Functionality
+// 		slideshow               :   1,			// Slideshow on/off
+// 		autoplay				:	1,			// Slideshow starts playing automatically
+// 		start_slide             :   1,			// Start slide (0 is random)
+// 		stop_loop				:	0,			// Pauses slideshow on last slide
+// 		random					: 	0,			// Randomize slide order (Ignores start slide)
+// 		slide_interval          :   12000,		// Length between transitions
+// 		transition              :   1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+// 		transition_speed		:	300,		// Speed of transition
+// 		new_window				:	1,			// Image links open in new window/tab
+// 		pause_hover             :   0,			// Pause slideshow on hover
+// 		keyboard_nav            :   1,			// Keyboard navigation on/off
+// 		performance				:	1,			// 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
+// 		image_protect			:	1,			// Disables image dragging and right click with Javascript
 												   
-		// Size & Position						   
-		min_width		        :   0,			// Min width allowed (in pixels)
-		min_height		        :   0,			// Min height allowed (in pixels)
-		vertical_center         :   1,			// Vertically center background
-		horizontal_center       :   1,			// Horizontally center background
-		fit_always				:	0,			// Image will never exceed browser width or height (Ignores min. dimensions)
-		fit_portrait         	:   1,			// Portrait images will not exceed browser height
-		fit_landscape			:   0,			// Landscape images will not exceed browser width
+// 		// Size & Position						   
+// 		min_width		        :   0,			// Min width allowed (in pixels)
+// 		min_height		        :   0,			// Min height allowed (in pixels)
+// 		vertical_center         :   1,			// Vertically center background
+// 		horizontal_center       :   1,			// Horizontally center background
+// 		fit_always				:	0,			// Image will never exceed browser width or height (Ignores min. dimensions)
+// 		fit_portrait         	:   1,			// Portrait images will not exceed browser height
+// 		fit_landscape			:   0,			// Landscape images will not exceed browser width
 												   
-		// Components							
-		slide_links				:	'blank',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
-		thumb_links				:	0,			// Individual thumb links for each slide
-		thumbnail_navigation    :   0,			// Thumbnail navigation
-		slides 					:  	[			// Slideshow Images
-											{image : '_include/img/slider-images/image01.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/image02.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/image03.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/image04.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''}  
-									],
+// 		// Components							
+// 		slide_links				:	'blank',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
+// 		thumb_links				:	0,			// Individual thumb links for each slide
+// 		thumbnail_navigation    :   0,			// Thumbnail navigation
+// 		slides 					:  	[			// Slideshow Images
+// 											{image : '_include/img/slider-images/image01.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''},
+// 											{image : '_include/img/slider-images/image02.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''},
+// 											{image : '_include/img/slider-images/image03.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''},
+// 											{image : '_include/img/slider-images/image04.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''}  
+// 									],
 									
-		// Theme Options			   
-		progress_bar			:	0,			// Timer for each slide							
-		mouse_scrub				:	0
+// 		// Theme Options			   
+// 		progress_bar			:	0,			// Timer for each slide							
+// 		mouse_scrub				:	0
 		
-	});
+// 	});
 
-}
+// }
 
 
 /* ==================================================
    Navigation Fix
 ================================================== */
 
-BRUSHED.nav = function(){
-	$('.sticky-nav').waypoint('sticky');
-}
+// BRUSHED.nav = function(){
+// 	$('.sticky-nav').waypoint('sticky');
+// }
 
 
 /* ==================================================
@@ -215,31 +215,31 @@ BRUSHED.contactForm = function(){
    Twitter Feed
 ================================================== */
 
-BRUSHED.tweetFeed = function(){
-	var valueTop = -64;
+// BRUSHED.tweetFeed = function(){
+// 	var valueTop = -64;
 	
-    $("#ticker").tweet({
-          username: "Bluxart",
-          page: 1,
-          avatar_size: 0,
-          count: 10,
-		  template: "{text}{time}",
-		  filter: function(t){ return ! /^@\w+/.test(t.tweet_raw_text); },
-          loading_text: "loading ..."
-	}).bind("loaded", function() {
-	  var ul = $(this).find(".tweet_list");
-	  var ticker = function() {
-		setTimeout(function() {
-			ul.find('li:first').animate( {marginTop: valueTop + 'px'}, 500, 'linear', function() {
-				$(this).detach().appendTo(ul).removeAttr('style');
-			});	
-		  ticker();
-		}, 5000);
-	  };
-	  ticker();
-	});
+//     $("#ticker").tweet({
+//           username: "Bluxart",
+//           page: 1,
+//           avatar_size: 0,
+//           count: 10,
+// 		  template: "{text}{time}",
+// 		  filter: function(t){ return ! /^@\w+/.test(t.tweet_raw_text); },
+//           loading_text: "loading ..."
+// 	}).bind("loaded", function() {
+// 	  var ul = $(this).find(".tweet_list");
+// 	  var ticker = function() {
+// 		setTimeout(function() {
+// 			ul.find('li:first').animate( {marginTop: valueTop + 'px'}, 500, 'linear', function() {
+// 				$(this).detach().appendTo(ul).removeAttr('style');
+// 			});	
+// 		  ticker();
+// 		}, 5000);
+// 	  };
+// 	  ticker();
+// 	});
 	
-}
+// }
 
 
 /* ==================================================
@@ -395,25 +395,25 @@ BRUSHED.toolTip = function(){
 	Init
 ================================================== */
 
-BRUSHED.slider();
+// BRUSHED.slider();
 
 $(document).ready(function(){
-	Modernizr.load([
-	{
-		test: Modernizr.placeholder,
-		nope: '_include/js/placeholder.js', 
-		complete : function() {
-				if (!Modernizr.placeholder) {
-						Placeholders.init({
-						live: true,
-						hideOnFocus: false,
-						className: "yourClass",
-						textColor: "#999"
-						});    
-				}
-		}
-	}
-	]);
+	// Modernizr.load([
+	// {
+	// 	test: Modernizr.placeholder,
+	// 	nope: '_include/js/placeholder.js', 
+	// 	complete : function() {
+	// 			if (!Modernizr.placeholder) {
+	// 					Placeholders.init({
+	// 					live: true,
+	// 					hideOnFocus: false,
+	// 					className: "yourClass",
+	// 					textColor: "#999"
+	// 					});    
+	// 			}
+	// 	}
+	// }
+	// ]);
 	
 	// Preload the page with jPreLoader
 	$('body').jpreLoader({
@@ -426,7 +426,7 @@ $(document).ready(function(){
 		}
 	});
 	
-	BRUSHED.nav();
+	//BRUSHED.nav();
 	BRUSHED.mobileNav();
 	BRUSHED.listenerMenu();
 	BRUSHED.menu();
@@ -435,7 +435,7 @@ $(document).ready(function(){
 	BRUSHED.filter();
 	BRUSHED.fancyBox();
 	BRUSHED.contactForm();
-	BRUSHED.tweetFeed();
+	//BRUSHED.tweetFeed();
 	BRUSHED.scrollToTop();
 	BRUSHED.utils();
 	BRUSHED.accordion();
